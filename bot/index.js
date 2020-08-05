5bf8fd93ad7fed9622e852052b4132a318c71138
Nikolaj Frey <lopudesigns@gmail.com>
2020-08-05 14:35:06 +1000

    Added secret config file

--- bot/index.js
+++ bot/index.js
@@ -2,3 +2,14 @@
 // Initialize
 console.log("Starting Facebook Waste Management chatbot")
 
+// ES5
+let FB = require('fb')
+
+let secret = require('secret')
+
+let options = {
+	appId: "2710167052529715",
+	secret
+}
+
+fb = new FB.Facebook(options);
