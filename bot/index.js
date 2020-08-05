9549550e7ef43e86709849de9f930ed22b69f68c
lopugit <ireply@myleisure.com.au>
2020-08-05 14:16:54 +1000

    Added template code for chatbot

--- bot/index.js
+++ bot/index.js
@@ -1,1 +1,15 @@
+
+// Initialize
 console.log("Starting Facebook Waste Management chatbot")
+
+// Initialize API through chat api library 
+const login = require("facebook-chat-api");
+
+// Create simple echo bot
+login({email: "FB_EMAIL", password: "FB_PASSWORD"}, (err, api) => {
+    if(err) return console.error(err);
+
+    api.listen((err, message) => {
+        api.sendMessage(message.body, message.threadID);
+    });
+});
